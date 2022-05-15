4d2024119dcd7edbaebde6146841cdd345f6dace
taekionic <98569426+taekionic@users.noreply.github.com>
2022-05-15 09:58:11 -0400

    Update Abstraction.py

--- Learning Files/Abstraction.py
+++ Learning Files/Abstraction.py
@@ -1,21 +1,21 @@
 from abc import ABC, abstractmethod
 
 class bankbalance(ABC):
-    def __init__(self):
+    def __init__(self): #defining a private variable that contains the bank balance of an account. 
         self.__privateBalance = 1000
-    def getBalance(self):
+    def getBalance(self): #defining a method to retrieve the private balance. 
         return self.__privateBalance
 
-    def funds(self, withdrawal):
-        print("Your withdrawl request was: ", withdrawal)
+    def funds(self, withdrawal): #defining a method to print the withdrawal request amount. This value will be passed to the abstract method below.
+        print("Your withdrawal request was: ", withdrawal)
 
-    @abstractmethod
+    @abstractmethod #this function is instructing us to pass an argument, but without any information on how or what kind of data.
     def withdrawalrequest(self, withdrawal):
         pass
 
 
 class Request(bankbalance):
-    def withdrawalrequest(self, withdrawal):
+    def withdrawalrequest(self, withdrawal): #here, we are defining how to implement the withdrawal request function from above, as well as retrieving our private balance and comparing the two values to return the difference. 
         self.bal = self.getBalance()
         self.difference = (withdrawal - self.bal)
         print(f'You do not have enough funds to make this withdrawl.\n Your balance is ${self.getBalance()}. \n Your withdrawal request was ${withdrawal}. \n To make this withdrawal, please deposit ${self.difference}')
